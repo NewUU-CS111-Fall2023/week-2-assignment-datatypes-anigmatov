cb1af6451432a15872710b675df5e5ac0138281f
anigmatov <127713674+anigmatov@users.noreply.github.com>
2023-10-25 23:09:49 +0500

    Updating main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,19 +1,38 @@
 /*
- * Author:
- * Date:
+ * Author: Abdusamad Nigmatov
+ * Date: 25.10.2023
  */
 
 #include <iostream>
 #include "task_1.h"
 
+class leapYear {
+public:
+    void leapYearValue(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            std::cout << year << " is a leap year." << std::endl;
+        } else {
+            std::cout << year << " is not a leap year." << std::endl;
+        }
+    }
+};
+
+
+
+using namespace std;
 int main() {
-    std::cout << "Task 1" << std::endl;
-    // call for task 1
-    std::cout << "Task 2" << std::endl;
+    leapYear year_1;
+    cout<<"Task_1"<<endl;
+    cout<<"Please enter a year: "<<endl;
+    int x;
+    cin>>x;
+    year_1.leapYearValue(x);
+
+    std::cout << "Task 2 - Python" << std::endl;
     // call for task 2
-    std::cout << "Task 3" << std::endl;
+    std::cout << "Task 3 - Python" << std::endl;
     // call for task 3
-    std::cout << "Task 4" << std::endl;
+    std::cout << "Task 4 - Python" << std::endl;
     // call for task 4
     std::cout << "Task 5" << std::endl;
     // call for task 5
