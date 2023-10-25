a02fb64b3b46081d9f28d0d251d66aa16f7bafe0
anigmatov <127713674+anigmatov@users.noreply.github.com>
2023-10-25 23:09:07 +0500

    Updating main.py

--- src/py/main.py
+++ src/py/main.py
@@ -1,2 +1,13 @@
-# * Author:
-# * Date:
+# * Author: Abdusamad Nigmatov
+# * Date: 25.10.2022
+
+from tasks import NumberGuesser, LongArithmeticCalculator, StringMatcher
+print("Task_2")
+number = NumberGuesser()
+number.guess_number(int(input("Please guess a number! \n")))
+print("Task_3")
+input_number = LongArithmeticCalculator()
+input_number.calculate_long_arithmetic(float(input("Please enter a number! \n")))
+print("Task_4")
+string = StringMatcher()
+string.match_string(input("Enter a sentence! \n").split(" "))
